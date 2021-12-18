ef32ba0d5e0e8949aac1c52379155ca3c8811cc9
Gene Hackerman <arias.rafael.1984@gmail.com>
2021-12-18 11:57:10 -0500

    adjusted js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
 window.alert("This is an alert! Javascript is running!");
+alert("This is an alert! Javascript is running!");
+
+// this creats a function named "fight" //
+function fight() {
+    window.alert("The fight has begun!");
+}
